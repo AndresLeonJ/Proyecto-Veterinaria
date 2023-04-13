caf1cb7a8442edbc70ab21b9e7fa810010a2c10d
AndresLeonJ <105888687+AndresLeonJ@users.noreply.github.com>
2023-04-12 18:55:21 -0600

    Dar en adopcion Entity

---
+++ src/main/java/com/veterinaria/entity/Dar_En_Adopcion.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.veterinaria.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Lion
+ */
+@Entity
+@Table(name = "darenadopcion")
+public class Dar_En_Adopcion implements Serializable {
+    private long id;
+    private String nombre;
+    private String tipo;
+    private int edad;
+    private String sexo;
+    private String raza;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+    
+    
+}
+
