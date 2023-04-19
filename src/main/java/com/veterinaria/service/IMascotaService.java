df0b682b7459fd11d09f869b336f9760b3e5c829
rodri0218 <105897998+rodri0218@users.noreply.github.com>
2023-04-18 21:09:20 -0600

    controller,rep y service d animal

---
+++ src/main/java/com/veterinaria/service/IMascotaService.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.veterinaria.service;
+
+import com.veterinaria.entity.Mascotas;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author user
+ */
+
+@Service
+public interface IMascotaService {
+  public List<Mascotas> getAllAnimales();
+
+    public Mascotas getAnimalById(long id);
+   
+    public void saveAnimal(Mascotas animal);
+
+    public void deleteAnimal(long id);
+
+   
+}
---
+++ src/main/java/com/veterinaria/service/MascotaService.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.veterinaria.service;
+
+import com.veterinaria.entity.Mascotas;
+import com.veterinaria.repository.AnimalRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MascotaService {
+    @Autowired
+    private AnimalRepository animalRepository;
+
+    public List<Mascotas> getAllAnimales() {
+        return (List<Mascotas>) animalRepository.findAll();
+    }
+
+    public Mascotas getAnimalById(Long idAnimal) {
+        return animalRepository.findById(idAnimal).orElseThrow(() -> new RuntimeException("Animal no encontrado"));
+    }
+
+    public void saveAnimal(Mascotas animal) {
+        animalRepository.save(animal);
+    }
+
+    public void deleteAnimal(Long idAnimal) {
+        Mascotas animal = animalRepository.findById(idAnimal).orElseThrow(() -> new RuntimeException("Animal no encontrado"));
+        animalRepository.delete(animal);
+    }
+
+    
+}
