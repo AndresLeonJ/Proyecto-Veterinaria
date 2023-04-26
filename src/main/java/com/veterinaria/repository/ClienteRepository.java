1675e121bd313a578c3078adb7ddf0750e75208b
rodri0218 <105897998+rodri0218@users.noreply.github.com>
2023-04-26 14:21:20 -0600

    webconfig

--- src/main/java/com/veterinaria/repository/ClienteRepository.java
+++ src/main/java/com/veterinaria/repository/ClienteRepository.java
@@ -5,9 +5,9 @@
 package com.veterinaria.repository;
 import com.veterinaria.entity.Cliente;
 import java.util.List;
-import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+
 
 
 public interface ClienteRepository extends CrudRepository<Cliente, Long> {
